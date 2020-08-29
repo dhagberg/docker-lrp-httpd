a6a28e97d73542befb742431123261339afafba0
D. J. Hagberg <dhagberg@millibits.com>
2020-08-29 23:24:08 +0000

    Add support for tagging/pushing to both docker hub and local registry

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 set -xe
-docker build -t dhagberg/co811-httpd .
+PLAIN_NAME=co811-httpd
+HUB_NAME=dhagberg/$PLAIN_NAME
+SVN_NAME=svn.co811.org:5000/$PLAIN_NAME
+
+docker build -t $HUB_NAME .
+
+# Tag and push if given
+if [ "$1" = "push" ]; then
+    docker push $HUB_NAME
+    docker tag  $HUB_NAME $SVN_NAME
+    docker push $SVN_NAME
+fi
