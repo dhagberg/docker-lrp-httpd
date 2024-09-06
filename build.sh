dde4829d0d2360ed3b5f1f8fef1753f3c30430b5
D. J. Hagberg <dhagberg@millibits.com>
2024-09-06 17:51:50 +0000

    oops.

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -xeuo pipefail
-PLAIN_NAME=lrp-httpd
+PLAIN_NAME=lrp-u-httpd
 HUB_NAME=dhagberg/$PLAIN_NAME
 CI_NAME=cga-ci:5000/$PLAIN_NAME
 
 docker build --pull -t $HUB_NAME .
 
 # Tag and push if given
-if [ "$1" = "push" ]; then
+if [ $# -gt 0 ]; then
+  if [ "$1" = "push" ]; then
     docker push $HUB_NAME
     docker tag  $HUB_NAME $CI_NAME
     docker push $CI_NAME
+  fi
 fi
